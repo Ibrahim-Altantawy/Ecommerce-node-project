00db585124910ad957e57b33f5b22f70496d335f
Ibrahim Altantawy <ibrahimaltantawy@yahoo.com>
2023-05-01 15:39:10 +0300

    add main route

--- src/index.router.js
+++ src/index.router.js
@@ -9,24 +9,24 @@ import authRouter from "./modules/auth/authRouter.js";
 import cartRouter from "./modules/cart/cartRouter.js";
 import orderRouter from "./modules/order/orderRouter.js";
 import productRouter from "./modules/product/productRouter.js";
-import cors from 'cors'
+import cors from "cors";
 const initApp = (app, express) => {
-    app.use(cors())
-//   const whiteList = ["http://127.0.0.1:5000"];
+  app.use(cors());
+  //   const whiteList = ["http://127.0.0.1:5000"];
 
-//   app.use(async (req, res, next) => {
-//     if (!whiteList.includes(req.header("origin"))) {
-//       return next(new Error("not allowed by cors", { cause: 403 }));
-//     }
-//     for (const origin of whiteList) {
-//       await res.header("Acess-Control-Allow-Origin", origin);
-//       break;
-//     }
-//     await res.header("Access-Control-Allow-Headers", "*");
-//     await res.header("Access-Control-Allow-Private-Networ", "true");
-//     await res.header("Access-Control-Allow-Methods", "*");
-//     next();
-//   });
+  //   app.use(async (req, res, next) => {
+  //     if (!whiteList.includes(req.header("origin"))) {
+  //       return next(new Error("not allowed by cors", { cause: 403 }));
+  //     }
+  //     for (const origin of whiteList) {
+  //       await res.header("Acess-Control-Allow-Origin", origin);
+  //       break;
+  //     }
+  //     await res.header("Access-Control-Allow-Headers", "*");
+  //     await res.header("Access-Control-Allow-Private-Networ", "true");
+  //     await res.header("Access-Control-Allow-Methods", "*");
+  //     next();
+  //   });
   app.use(express.json());
   app.use("/auth", authRouter);
   app.use("/user", authRouter);
@@ -37,7 +37,12 @@ const initApp = (app, express) => {
   app.use("/subCategory", SubCategoryRouter);
   app.use("/cupon", cuponRouter);
   app.use("/brand", brandRouter);
-
+  app.use("/", productRouter);
+  app.use("*", (req, res, next) => {
+    return res
+      .status(404)
+      .json({ message: "In-Valid router please check you rl" });
+  });
   app.use(globalError);
   dbConnect();
 };
